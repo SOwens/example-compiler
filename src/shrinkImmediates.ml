3910a17e1149ba957cb756c683f1938ac347ea02
Scott Owens <S.A.Owens@kent.ac.uk>
2016-01-20 11:10:33 +0000

    GPL it

--- src/shrinkImmediates.ml
+++ src/shrinkImmediates.ml
@@ -1,3 +1,21 @@
+(*
+ * Example compiler
+ * Copyright (C) 2015-2016 Scott Owens
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*)
+
 (* Ensure that all immediate arguments fit into 32 bits.  We assume that
    constant propagation has ensured that no operation has two immediate
    arguments, and we maintain that property here. *)
