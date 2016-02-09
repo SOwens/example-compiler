e45972bde5a170ac4743cf22e93d588f68ab9253
Scott Owens <S.A.Owens@kent.ac.uk>
2016-02-09 16:17:00 +0000

    Make array allocation C program standards compliant
    Use unions instead of casting between pointers and ints.

--- runtime/array.c
+++ runtime/array.c
@@ -25,71 +25,80 @@
 #include <stdlib.h>
 #include <assert.h>
 
-#define S sizeof(int64_t)
+/* Since the first element of an array will have a different type than the
+ * rest, and the last dimension's array will have numbers rather than pointers. */
+union elt {
+  uint64_t num;
+  union elt *ptr;
+};
+
+typedef union elt elt;
+
+#define S sizeof(elt)
 
 /* A 1 dimensional array */
-int64_t* allocate1(int64_t dim1) {
-  int64_t* x = malloc(S*(dim1+1));
-  for (int64_t i = 0; i < dim1; i++)
-    x[i+1] = 0;
-  x[0] = dim1;
+elt* allocate1(int64_t dim1) {
+  elt* x = malloc(S*(dim1+1));
+  for (unsigned long i = 0; i < dim1; i++)
+    x[i+1].num = 0;
+  x[0].num = dim1;
   return x;
 }
 
 /* A 2 dimensional array is an array of arrays */
-int64_t* allocate2(int64_t dim1, int64_t dim2) {
-  int64_t** x = malloc(S*(dim1+1));
-  for (int64_t i = 0; i < dim1; i++) {
-    int64_t* y = malloc(S*(dim2+1));
-    x[i+1] = y;
+elt* allocate2(int64_t dim1, int64_t dim2) {
+  elt* x = malloc(S*(dim1+1));
+  for (unsigned long i = 0; i < dim1; i++) {
+    elt* y = malloc(S*(dim2+1));
+    x[i+1].ptr = y;
     for (int64_t j = 0; j < dim2; j++)
-      y[j+1] = 0;
-    y[0] = dim2;
+      y[j+1].num = 0;
+    y[0].num = dim2;
   }
-  ((int64_t*)x)[0] = dim1;
-  return (int64_t*) x;
+  x[0].num = dim1;
+  return x;
 }
 
 /* Allocate dimensions dim to num_dim where the length of each dimension is
  * given by the list dims[] */
-int64_t* allocate_n_help(int64_t dim, int64_t num_dim, int64_t dims[]) {
+elt* allocate_n_help(int64_t dim, int64_t num_dim, int64_t dims[]) {
   if (dim == num_dim - 1)
     return allocate1(dims[dim]);
   else {
-    int64_t** x = malloc(S*(dims[dim]+1));
-    for (int i = 0; i < dims[dim]; i++) {
-      x[i+1] = allocate_n_help(dim+1, num_dim, dims);
+    elt* x = malloc(S*(dims[dim]+1));
+    for (unsigned long i = 0; i < dims[dim]; i++) {
+      x[i+1].ptr = allocate_n_help(dim+1, num_dim, dims);
     }
-    ((int64_t*)x)[0] = dims[dim];
-    return (int64_t*) x;
+    x[0].num = dims[dim];
+    return x;
   }
 }
 
-int64_t* allocate_n(int64_t num_dim, int64_t dims[]) {
+elt* allocate_n(int64_t num_dim, int64_t dims[]) {
   return allocate_n_help(0, num_dim, dims);
 }
 
-int64_t* allocate3(int64_t dim1, int64_t dim2, int64_t dim3) {
+elt* allocate3(int64_t dim1, int64_t dim2, int64_t dim3) {
   int64_t dims[] = {dim1, dim2, dim3};
   return allocate_n(3, dims);
 }
 
-int64_t* allocate4(int64_t dim1, int64_t dim2, int64_t dim3, int64_t dim4) {
+elt* allocate4(int64_t dim1, int64_t dim2, int64_t dim3, int64_t dim4) {
   int64_t dims[] = {dim1, dim2, dim3, dim4};
   return allocate_n(4, dims);
 }
 
-int64_t* allocate5(int64_t dim1, int64_t dim2, int64_t dim3, int64_t dim4, int64_t dim5) {
+elt* allocate5(int64_t dim1, int64_t dim2, int64_t dim3, int64_t dim4, int64_t dim5) {
   int64_t dims[] = {dim1, dim2, dim3, dim4, dim5};
   return allocate_n(5, dims);
 }
 
-int64_t* allocate6(int64_t dim1, int64_t dim2, int64_t dim3, int64_t dim4, int64_t dim5, int64_t dim6) {
+elt* allocate6(int64_t dim1, int64_t dim2, int64_t dim3, int64_t dim4, int64_t dim5, int64_t dim6) {
   int64_t dims[] = {dim1, dim2, dim3, dim4, dim5, dim6};
   return allocate_n(6, dims);
 }
 
-int64_t* allocate7(int64_t dim1, int64_t dim2, int64_t dim3, int64_t dim4, int64_t dim5, int64_t dim6, int64_t dim7) {
+elt* allocate7(int64_t dim1, int64_t dim2, int64_t dim3, int64_t dim4, int64_t dim5, int64_t dim6, int64_t dim7) {
   int64_t dims[] = {dim1, dim2, dim3, dim4, dim5, dim6, dim7};
   return allocate_n(7, dims);
 }
