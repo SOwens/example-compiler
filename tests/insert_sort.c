f384a159838b67fb3443a7cad0c5e2395206cd62
Scott Owens <S.A.Owens@kent.ac.uk>
2017-02-11 16:47:56 +0000

    Make C insert-sort more like the example

--- tests/insert_sort.c
+++ tests/insert_sort.c
@@ -4,17 +4,10 @@
 extern int64_t input();
 extern void output(int64_t i);
 
-int main(int argc, char** argv) {
-  int64_t len = input();
-  int64_t *a = malloc(len * sizeof(int64_t));
-  unsigned int i = 0;
-  while (i < len) {
-    int64_t y = input();
-    a[i] = y;
-    i = i + 1;
-  }
-
-  i = 1;
+int sort(int64_t a[], int64_t len) {
+  int64_t i = 1;
+  int64_t y = 1;
+  int64_t j = 1;
   while (i < len) {
     int64_t y = a[i];
     int64_t j = i - 1;
@@ -25,6 +18,23 @@ int main(int argc, char** argv) {
     a[j+1] = y;
     i = i + 1;
   }
+  return i;
+}
+
+int64_t test(int64_t i2) {
+  int64_t len = 0;
+  int64_t *a;
+  int64_t i = 0;
+  int64_t y = 0;
+  len = input();
+  a = malloc(len * sizeof(int64_t));
+  while (i < len) {
+    int64_t y = input();
+    a[i] = y;
+    i = i + 1;
+  }
+
+  i = sort(a,len);
 
   i = 0;
   while (i < len) {
@@ -32,5 +42,12 @@ int main(int argc, char** argv) {
     output(y);
     i = i + 1;
   }
+  return i;
+}
+
+int64_t r;
+
+int main(int argc, char** argv) {
+  r = test(1);
   return 0;
 }
